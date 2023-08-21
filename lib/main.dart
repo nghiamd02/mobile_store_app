8ba199c1880e05ae255f2bb9f898c176288750c5
nghiamd <nghiamd002@gmail.com>
2023-08-21 14:34:37 +0700

    create static botton nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndexPage = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
           child: Text('Hello Mobile Store App'),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.arrow_back_ios_new_sharp), label: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_rounded), label: ""),
+          ],
+          currentIndex: _currentIndexPage,
+          onTap: (value) {
+            setState(() {
+              _currentIndexPage = value;
+            });
+          },
         ),
       ),
     );
