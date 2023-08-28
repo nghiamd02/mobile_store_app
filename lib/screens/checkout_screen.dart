42e234d8ada2e042496d4c4d95a5231fcc21ffde
Trinh Binh Minh <minhtbse151276@fpt.edu.vn>
2023-08-28 17:07:20 +0700

    UpdateUICheckout

--- lib/screens/checkout_screen.dart
+++ lib/screens/checkout_screen.dart
@@ -30,7 +30,7 @@ class _CheckoutPageState extends State<CheckoutPage> {
               child: Column(
             children: [
               const SizedBox(
-                height: 250,
+                height: 200,
               ),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -133,95 +133,116 @@ class _CheckoutPageState extends State<CheckoutPage> {
                     ),
                   ),
                   const SizedBox(
-                height: 30,
-              ),
-              Container(
-                color: const Color.fromARGB(255, 255, 1, 9),
-                padding: const EdgeInsets.symmetric(
-                  vertical:
-                      100, // Điều chỉnh giá trị vertical để thay đổi khoảng cách trên và dưới
-                  horizontal:
-                      100, // Điều chỉnh giá trị horizontal để thay đổi khoảng cách trái và phải
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 10),
-                child:  Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Select form to pay:",
-                      style: TextStyle(
-                        fontFamily: "Inter",
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
-                        color: Color(0xff000000),
-                      ),
-                      textAlign: TextAlign.left,
+                    height: 30,
+                  ),
+                  Container(
+                    color: const Color.fromARGB(255, 255, 1, 9),
+                    padding: const EdgeInsets.symmetric(
+                      vertical:
+                          100, // Điều chỉnh giá trị vertical để thay đổi khoảng cách trên và dưới
+                      horizontal:
+                          100, // Điều chỉnh giá trị horizontal để thay đổi khoảng cách trái và phải
                     ),
-                    const SizedBox(height: 5,),
-                   RadioListTile<int>(
-                      title: Row(
-                        children: [
-                          Image.asset('assets/img/image 15.png'),
-                          const SizedBox(width: 10,),
-                          const Text("Wallet Momo"),
-                        ],
-                      ),
-                      value: 0,
-                      groupValue: selectedPaymentOption,
-                      onChanged: (value) {
-                        setState(() {
-                          selectedPaymentOption = value!;
-                        });
-                        
-                      },
-                      activeColor: Color(0xFF5BB85D)
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Select form to pay:",
+                          style: TextStyle(
+                            fontFamily: "Inter",
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: Color(0xff000000),
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        RadioListTile<int>(
+                            title: Row(
+                              children: [
+                                Image.asset('assets/img/image 15.png'),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                const Text("Wallet Momo"),
+                              ],
+                            ),
+                            value: 0,
+                            groupValue: selectedPaymentOption,
+                            onChanged: (value) {
+                              setState(() {
+                                selectedPaymentOption = value!;
+                              });
+                            },
+                            activeColor: Color(0xFF5BB85D)),
+                        RadioListTile<int>(
+                            title: Row(
+                              children: [
+                                Image.asset('assets/img/money 1.png'),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                const Text("Pay after receive order"),
+                              ],
+                            ),
+                            value: 1,
+                            groupValue: selectedPaymentOption,
+                            onChanged: (value) {
+                              setState(() {
+                                selectedPaymentOption = value!;
+                              });
+                            },
+                            activeColor: Color(0xFF5BB85D)),
+                        RadioListTile<int>(
+                          title: Row(
+                            children: [
+                              Image.asset('assets/img/debit-card 1.png'),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              const Text("Banking"),
+                            ],
+                          ),
+                          value: 2,
+                          groupValue: selectedPaymentOption,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedPaymentOption = value!;
+                            });
+                          },
+                          activeColor: Color(0xFF5BB85D),
+                        ),
+                      ],
                     ),
-
-                    RadioListTile<int>(
-                      title: Row(
-                        children: [
-                          Image.asset('assets/img/money 1.png'),
-                          const SizedBox(width: 10,),
-                          const Text("Pay after receive order"),
-                        ],
-                      ),
-                      value: 1,
-                      groupValue: selectedPaymentOption,
-                      onChanged: (value) {
-                        setState(() {
-                          selectedPaymentOption = value!;
-                        });
-                      },
-                      activeColor: Color(0xFF5BB85D)
-                    ),
-
-                    RadioListTile<int>(
-                      title: Row(
-                        children: [
-                          Image.asset('assets/img/debit-card 1.png'),
-                          const SizedBox(width: 10,),
-                          const Text("Banking"),
-                        ],
-                      ),
-                      value: 2,
-                      groupValue: selectedPaymentOption,
-                      onChanged: (value) {
-                        setState(() {
-                          selectedPaymentOption = value!;
-                        });
-                      },
-                      activeColor: Color(0xFF5BB85D),
-                    ),
-                    
-                  ],
-                ),
-              ),
+                  ),
                 ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Xử lý khi nhấn nút Pay
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF5BB85D), // Màu nền xanh
+                  minimumSize:
+                      const Size(400, 50), // Đặt chiều rộng là full width
+                ),
+                child: const Text(
+                  "Pay",
+                  style: TextStyle(
+                    fontFamily: "Inter",
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ],
           ))),
